4304850358829ba33ac015bf10f7274b80ef11fa
Forklein <giuseppepisani2@gmail.com>
2021-07-02 15:05:21 +0200

    Set variable

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 console.log('Ciao Peppe');
 
 
-// Traccia
-
 /* Mail
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
+*/
 
+var listaccess = ['forklein@gmail.com', 'ciccio@gmail.com', 'pippo@gmail.com', 'marcolanci@gmail.com'];
+var mailuser = prompt('Inserisci la tua email: ');
+
+var canaccess = true;
+
+
+
+if(listaccess.length - 1 !== mailuser){
+    console.log('La tua mail può accedere');
+}else{
+    console.log('non puoi accedere');
+}
+
+
+
+
+/*
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
 */
+
+
